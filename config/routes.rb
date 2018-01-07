21191e8237a3230f7a7b894e59885741406de0c5
christelva <cvanaanholt@hotmail.com>
2018-01-07 13:51:25 +0100

    put songs list in table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "artists#index"
 
   devise_for :users
-  resources :artists
-  resources :songs
+  resources :artists do
+    resources :songs
+  end
 end
